57ab68eef7d6a9c571c3c8ea1c05387e3d104450
Arjan Koning <arjankoning1@gmail.com>
2025-08-14 17:45:02 +0200

    version

--- source/mainout.f90
+++ source/mainout.f90
@@ -6,10 +6,10 @@ subroutine mainout
 ! Author    : Arjan Koning
 !
 ! 2023-12-29: Original code
-! 2025-05-20: Current revision
+! 2025-08-14: Current revision
 !-----------------------------------------------------------------------------------------------------------------------------------
 !
-  write(*,'(/"    TEFAL-2.1 (Version: May 20, 2025)"/)')
+  write(*,'(/"    TEFAL-2.13 (Version: August 14, 2025)"/)')
   write(*, '(10x, " Creating ENDF-6 files with TALYS")')
   write(*, '(/" Copyright (C) 2025  A.J. Koning     ")')
 !
