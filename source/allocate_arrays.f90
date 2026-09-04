0e691b8652b4c7c5509b8e51d8f9fee21578f959
Arjan Koning <arjankoning1@gmail.com>
2026-09-04 15:01:06 +0200

    update

--- source/allocate_arrays.f90
+++ source/allocate_arrays.f90
@@ -6,6 +6,7 @@ subroutine allocate_arrays
 !
   use A0_tefal_mod
   implicit none
+  integer:: Nencovtot
 !
   allocate(Ehist(0:idnum,1:numenspec,0:numpar,0:numen2))
   Ehist = 0.
@@ -13,5 +14,15 @@ subroutine allocate_arrays
   f0ex = 0.
   allocate(specexcl(0:idnum,1:numenspec,0:numpar,0:numen2))
   specexcl = 0.
+  if (flagcovar) then
+    Nencovtot = 1 + Nencov * Nencov
+    allocate(b33read(Nchancov,Nchancov,Nencovtot))
+    allocate(b33(Nchancov,Nchancov,Nencovtot))
+    allocate(b33MT(idnum,Nencovtot))
+    allocate(b33ZA(idnum,Nencovtot))
+    allocate(b8(idnum,Nencovtot))
+    allocate(b33MTread(idnum,Nencovtot))
+    allocate(b8read(idnum,Nencovtot))
+  endif
   return
 end subroutine allocate_arrays
