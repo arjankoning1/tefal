dbf99b299c217df71a8ac31d7005142f04752d78
Arjan Koning <arjankoning1@gmail.com>
2026-09-04 15:01:10 +0200

    update

--- source/deallocate_arrays.f90
+++ source/deallocate_arrays.f90
@@ -10,6 +10,12 @@ subroutine deallocate_arrays
   if (allocated(Ehist)) deallocate(Ehist)
   if (allocated(f0ex)) deallocate(f0ex)
   if (allocated(specexcl)) deallocate(specexcl)
+  if (allocated(b33read)) deallocate(b33read)
+  if (allocated(b33)) deallocate(b33)
+  if (allocated(b33MT)) deallocate(b33MT)
+  if (allocated(b33ZA)) deallocate(b33ZA)
+  if (allocated(b33MTread)) deallocate(b33MTread)
+  if (allocated(b8read)) deallocate(b8read)
 !
   return
 end subroutine deallocate_arrays
