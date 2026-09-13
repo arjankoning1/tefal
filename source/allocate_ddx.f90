a1be1c1f1af7fcecd302c3e02ff166afb710dbcd
Arjan Koning <arjankoning1@gmail.com>
2026-09-13 17:19:57 +0200

    update

---
+++ source/allocate_ddx.f90
@@ -0,0 +1,19 @@
+subroutine allocate_ddx
+!
+!-----------------------------------------------------------------------------------------------------------------------------------
+! Purpose   : Allocate arrays for double-differential spectra
+!-----------------------------------------------------------------------------------------------------------------------------------
+!
+  use A0_tefal_mod
+  implicit none
+
+  if (.not. flagtabddx) return
+
+  allocate(ddxemis(1:2,1:Nenspec,1:numddx,0:numen2))
+  allocate(f0ddx(1:2,1:Nenspec,1:numddx,0:numen2))
+
+  ddxemis = 0.
+  f0ddx = 0.
+
+  return
+end subroutine allocate_ddx
